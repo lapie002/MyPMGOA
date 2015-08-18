dbbd703e896215ddeae99dfa9783e1ef3c0c807f
Bruno <lapierre.bruno@gmail.com>
2015-08-18 14:58:28 -0500

    message: edit product function and index.html

--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+//helper function
+
 function redirect($location)
 {
 	header("LOCATION: $location");
@@ -35,7 +37,42 @@ function fetch_array($result)
 	return mysqli_fetch_array($result);
 }
 
+//get products 
 
+ function get_products()
+ {
+	$result = query("SELECT * FROM products");
+	
+	confirm($result);
+	
+	while($row = fetch_array($result))
+	{
+		$product = <<<DELIMETER
+
+		<div class="col-sm-4 col-lg-4 col-md-4">
+			<div class="thumbnail">
+			<!-- actual image tag -->
+			<a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+			<!-- future image tag -->
+			<!-- <img src="/resources/...?add={$row['product_id']}" alt=""> -->
+			
+				<div class="caption">
+					<h4 class="pull-right">&#36;{$row['product_price']}</h4>
+					<h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a></h4>
+					<p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+					<a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
+				</div>
+			</div>
+		</div>
+
+DELIMETER;
+
+		echo $product;
+
+	}
+	
+	
+ }
 
 
 
